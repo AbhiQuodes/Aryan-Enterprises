2fa62fb83bfda9ca5e67c140a2a893530744853a
AbhiQuodes <bhartiabhinav618@gmail.com>
2024-12-31 09:32:07 +0530

    16th commit

--- src/components/ContactContainer.js
+++ src/components/ContactContainer.js
@@ -76,7 +76,7 @@ const ContactContainer = () => {
           >
             <form className="contact-box-form" onSubmit={sendInitialEmail}>
               <h4 className="contact-head">Quick Contact</h4>
-              {alertt && <div color="success">{alertt}</div>}
+              {alertt && <div style={{fontSize:"18px"}} color="success">{alertt}</div>}
               <input
                 type="email"
                 name="from_email"
