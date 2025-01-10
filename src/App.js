247657a1101a25ad3ede35f29ab4dc0858649b9b
AbhiQuodes <bhartiabhinav618@gmail.com>
2025-01-10 12:54:46 +0530

    39th commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import DashboardPage from "./pages/DashboardPage.js";
@@ -9,8 +9,30 @@ import AchievementPage from "./pages/AchievementPage.js";
 
 
 function App() {
+  const [deferredPrompt ,setDeferredPrompt] = useState(null);
+  useEffect(()=>{
+    window.addEventListener("beforeinstallprompt",(e)=>{
+      e.preventDefault();
+      setDeferredPrompt(e);
+    });
+  },[]);
+
+  const handleInstall =()=>{
+    if(deferredPrompt){
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult)=>{
+        if(choiceResult.outcome === "accepted")
+        {
+          console.log("User accepted the install prompt");
+
+        }
+        setDeferredPrompt(null);
+      });
+    }
+  };
   return (
     <div className="App">
+      
       <Routes>
         <Route path="/" element={<DashboardPage />}></Route>
         <Route path="/achievements" element={<AchievementPage />}></Route>
@@ -18,6 +40,14 @@ function App() {
         <Route path="/contact-us" element={<ContactUspage />}></Route>
         <Route path="/products" element={<ProductPage />}></Route>
       </Routes>
+      <div>
+{/*         <h1>Welcome to Aryan Enterprises</h1> */}
+        {
+          deferredPrompt &&(
+            <button onClick={handleInstall}> Install App</button>
+          )
+        }
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+if("serviceWorker" in navigator){
+  window.addEventListener("load",()=>{
+    navigator.serviceWorker.register("/serviceWorker.js").then(()=>{
+      console.log("service Worker registered !")
+    }).catch((error)=>{
+      console.log("service Worker registration failed : ",error)
+    });
+  })
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
