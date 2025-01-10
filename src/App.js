441dbe8f6dfb13c3b601de83f8405033369b9a60
AbhiQuodes <bhartiabhinav618@gmail.com>
2025-01-10 14:22:30 +0530

    42th commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,32 @@ import ContactUspage from "./pages/ContactUsPage.js";
 import ClientPage from "./pages/ClientPage.js";
 import ProductPage from "./pages/ProductPage.js";
 import AchievementPage from "./pages/AchievementPage.js";
-
+import appLogoImg from './images/appLogo.png'
 
 function App() {
+  const displayWidth = window.innerWidth;
   const [deferredPrompt ,setDeferredPrompt] = useState(null);
+  const [showInstallBox,setShowInstallBox]=useState(false);
   useEffect(()=>{
     window.addEventListener("beforeinstallprompt",(e)=>{
       e.preventDefault();
       setDeferredPrompt(e);
     });
   },[]);
+
+  const handleInstallDisplay=()=>{
+    if(showInstallBox)
+    {
+      setShowInstallBox(false);
+    }
+
+  }
+    if(displayWidth<= 600)
+    {
+      setTimeout(()=>{
+        setShowInstallBox(true)
+      }, 1000); 
+    }
 
   const handleInstall =()=>{
     if(deferredPrompt){
@@ -32,7 +48,7 @@ function App() {
   };
   return (
     <div className="App">
-      
+      <div onClick={handleInstallDisplay}>
       <Routes>
         <Route path="/" element={<DashboardPage />}></Route>
         <Route path="/achievements" element={<AchievementPage />}></Route>
@@ -40,12 +56,16 @@ function App() {
         <Route path="/contact-us" element={<ContactUspage />}></Route>
         <Route path="/products" element={<ProductPage />}></Route>
       </Routes>
+      </div>
       <div>
-        <h1>Welcome to Aryan Enterprises</h1>
-        {alert(deferredPrompt)}
         {
-                    deferredPrompt &&(
-            <button onClick={handleInstall}> Install App</button>
+          deferredPrompt && showInstallBox &&(
+            
+            <div className="install-box">
+              <img className="install-img" src={appLogoImg} alt='app-logo'></img>
+              <h1 className="install-title"> Aryan Enterprises</h1>
+            <button className="install-btn" onClick={handleInstall}> Install App</button>
+            </div>
           )
         }
       </div>
